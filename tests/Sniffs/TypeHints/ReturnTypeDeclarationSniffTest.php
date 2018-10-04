9d108f83063749ab854c789498aaad011ac78049
Björn Lange <code@b3nl.de>
2018-10-04 10:38:39 +0200

    PHPCS-73 Updated the ReturnTypeDeclarationSniffTest to the new unit test format

--- tests/Sniffs/TypeHints/ReturnTypeDeclarationSniffTest.php
+++ tests/Sniffs/TypeHints/ReturnTypeDeclarationSniffTest.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace BestIt\Sniffs\TypeHints;
 
 use BestIt\SniffTestCase;
-use PHP_CodeSniffer\Files\File;
+use BestIt\Sniffs\DefaultSniffIntegrationTestTrait;
 
 /**
  * Test for ReturnTypeDeclarationSniff
@@ -15,64 +15,5 @@ use PHP_CodeSniffer\Files\File;
  */
 class ReturnTypeDeclarationSniffTest extends SniffTestCase
 {
-    /**
-     * Test type hints with no errors.
-     *
-     * @return void
-     */
-    public function testCorrectTypeHints()
-    {
-        $this->assertNoSniffErrorInFile(
-            $this->checkFile($this->getFixtureFilePath('Correct.php'))
-        );
-    }
-
-    /**
-     * Test missing type hints and fix.
-     *
-     * @return void
-     */
-    public function testNoTypeHints()
-    {
-        $report = $this->checkFile($this->getFixtureFilePath('NoTypeHints.php'));
-
-        $this->assertSniffError(
-            $report,
-            8,
-            ReturnTypeDeclarationSniff::CODE_MISSING_RETURN_TYPE_HINT
-        );
-
-        $this->assertSniffError(
-            $report,
-            16,
-            ReturnTypeDeclarationSniff::CODE_MISSING_RETURN_TYPE_HINT
-        );
-
-        $this->assertSniffError(
-            $report,
-            24,
-            ReturnTypeDeclarationSniff::CODE_MISSING_RETURN_TYPE_HINT
-        );
-
-        $this->assertAllFixedInFile($report);
-    }
-
-    /**
-     * Checks the given file with defined error codes.
-     *
-     * @param string $file Filename of the fixture
-     * @param array $sniffProperties Array of sniff properties
-     *
-     * @return File The php cs file
-     */
-    protected function checkFileAgainstSniff(string $file, array $sniffProperties = []): File
-    {
-        return $this->checkFile(
-            $file,
-            $sniffProperties,
-            [
-                ReturnTypeDeclarationSniff::CODE_MISSING_RETURN_TYPE_HINT
-            ]
-        );
-    }
+    use DefaultSniffIntegrationTestTrait;
 }
