05979afeb21b2479864f1346992f4442f1288ff9
Björn Lange <code@b3nl.de>
2018-10-30 14:42:47 +0100

    PHPCS-155 Added the sniff to check for to much whitespace between star and tag

---
+++ tests/Sniffs/Commenting/Fixtures/RedundantWhitespaceSniff/correct/AbstractDocSniffTest.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BestIt\Sniffs\Commenting;
+
+use BestIt\SniffTestCase;
+use BestIt\Sniffs\DefaultSniffIntegrationTestTrait;
+use BestIt\Sniffs\TestTokenRegistrationTrait;
+use PHP_CodeSniffer\Sniffs\Sniff;
+use function defined;
+
+/**
+ * Basic test for the summary sniffs.
+ *
+ * @author blange <bjoern.lange@bestit-online.de>
+ * @package BestIt\Sniffs\Commenting
+ */
+abstract class AbstractDocSniffsTest extends SniffTestCase
+{
+    use DefaultSniffIntegrationTestTrait;
+    use TestTokenRegistrationTrait;
+
+    /**
+     * The tested class.
+     *
+     * We use this var to reduce the hard dependencies on internals from a specific slevomat version.
+     * - String 1
+     * - String 2
+     *
+     * @var Sniff|void
+     */
+    protected $fixture;
+
+    /**
+     * Returns the names of the required constants.
+     *
+     * @return array
+     */
+    public function getRequiredConstantAsserts(): array
+    {
+        return [
+            'CODE_NO_LINE_AFTER_DOC_COMMENT' => ['CODE_NO_LINE_AFTER_DOC_COMMENT', 'NoLineAfterDocComment'],
+            'CODE_NO_SUMMARY' => ['CODE_NO_SUMMARY', 'NoSummary'],
+            'CODE_SUMMARY_TOO_LONG' => ['CODE_SUMMARY_TOO_LONG', 'SummaryTooLong'],
+            'CODE_DOC_COMMENT_UC_FIRST' => ['CODE_DOC_COMMENT_UC_FIRST', 'DocCommentUcFirst'],
+        ];
+    }
+
+    /**
+     * Checks if the api is extended.
+     *
+     * @dataProvider getRequiredConstantAsserts
+     * @param string $constant The name of the constant.
+     *
+     * @return void
+     */
+    public function testRequiredConstants(string $constant): void
+    {
+        static::assertTrue(
+
+            defined(get_class($this->fixture) . '::' . $constant),
+            'Constant ' . $constant . ' is missing.'
+        );
+    }
+}
---
+++ tests/Sniffs/Commenting/RedundantWhitespaceSniffTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BestIt\Sniffs\Commenting;
+
+use BestIt\SniffTestCase;
+use BestIt\Sniffs\DefaultSniffIntegrationTestTrait;
+use BestIt\Sniffs\TestTokenRegistrationTrait;
+use BestIt\TestRequiredConstantsTrait;
+use const T_DOC_COMMENT_STAR;
+
+/**
+ * Class RedundantWhitespaceSniffTest
+ *
+ * @author blange <bjoern.lange@bestit-online.de>
+ * @package BestIt\Sniffs\Commenting
+ */
+class RedundantWhitespaceSniffTest extends SniffTestCase
+{
+    use DefaultSniffIntegrationTestTrait;
+    use TestTokenRegistrationTrait;
+    use TestRequiredConstantsTrait;
+
+    /**
+     * Returns the names of the required constants.
+     *
+     * @return array The required constants of a class. The second value is a possible value which should be checked.
+     */
+    public function getRequiredConstantAsserts(): array
+    {
+        return [
+            'CODE_ERROR_REDUNDANT_WHITESPACE' => ['CODE_ERROR_REDUNDANT_WHITESPACE', 'RedundantWhitespace']
+        ];
+    }
+
+    /**
+     * Returns the tokens which should be checked.
+     *
+     * @return array Returns the expected token ids.
+     */
+    protected function getExpectedTokens(): array
+    {
+        return [
+            T_DOC_COMMENT_STAR
+        ];
+    }
+
+    /**
+     * Sets upo the test.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->fixture = new RedundantWhitespaceSniff();
+    }
+}
