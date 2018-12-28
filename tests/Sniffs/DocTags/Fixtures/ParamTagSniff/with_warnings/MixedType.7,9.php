33683aa04680235301d515caa9704c2115346bea
Björn Lange <code@b3nl.de>
2018-12-28 21:47:55 +0100

    PHPCS-156 Extended unit tests to support ... for parameter lists

--- tests/Sniffs/DocTags/Fixtures/ParamTagSniff/with_warnings/MixedType.7,9.php
+++ tests/Sniffs/DocTags/Fixtures/ParamTagSniff/with_warnings/MixedType.7,9.php
@@ -6,8 +6,9 @@ class ClassWithErrorMixedType {
      *
      * @param mixed $bar
      * @param string $baz Baz!
+     * @param mixed $callbacks Possible callbacks.
      */
-    public function foo($bar, string $baz)
+    public function foo($bar, string $baz, ...$callbacks)
     {
         var_dump($bar, $baz);
     }
