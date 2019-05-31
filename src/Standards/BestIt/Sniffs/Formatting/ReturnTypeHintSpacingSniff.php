edbd5acc85a169bf30c6c426e86b4c8c8838961c
Georgi Damyanov <georgi.damyanov@bestit-online.at>
2019-05-31 14:09:00 +0200

    PHPCS-242 No whitespace allowed before return type colon (backport)
    Signed-off-by: Björn Lange <code@b3nl.de>

---
+++ src/Standards/BestIt/Sniffs/Formatting/ReturnTypeHintSpacingSniff.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BestIt\Sniffs\Formatting;
+
+use SlevomatCodingStandard\Sniffs\TypeHints\ReturnTypeHintSpacingSniff as BaseReturnTypeHintSpacingSniff;
+
+/**
+ * Checks that there is no whitespace before the  return type.
+ *
+ * @author blange <bjoern.lange@bestit-online.de>
+ * @package BestIt\Sniffs\Formatting
+ */
+class ReturnTypeHintSpacingSniff extends BaseReturnTypeHintSpacingSniff
+{
+
+}
